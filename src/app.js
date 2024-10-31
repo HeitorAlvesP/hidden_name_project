3a36ab22a09fe6216c1646b5e44ea78361d8b14b
Heitor Alves Pinto <heitorpinto.oficial@gmail.com>
2024-10-31 13:48:58 -0300

    bobiei dmais aqui sorry

--- src/app.js
+++ src/app.js
@@ -1,10 +1,7 @@
-const express = require('express');
-const app = express();
-app.use(express.json());
+import express from 'express';
 
-
-
-
+const app = express()
+app.use(express.json())
 
 
 app.listen(3000, () => console.log('API RODANDO EM PORT 3000'));
