2d870fc496a178ae5a92266fa353e00fd58dc0a3
Heitor Alves Pinto <heitorpinto.oficial@gmail.com>
2024-11-01 11:19:53 -0300

    delet prisma, não gosto desse trem n

--- src/app.js
+++ src/app.js
@@ -3,5 +3,7 @@ import express from 'express';
 const app = express()
 app.use(express.json())
 
+// app.post('/criar_conta', criar_conta)
+
 
 app.listen(3000, () => console.log('API RODANDO EM PORT 3000'));
